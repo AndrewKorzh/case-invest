15afb0dcde3e01072868ca2d57c6dd23c318ea49
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2025-03-03 10:51:04 +0300

    add check
    Добавлена проверка всех файлов

--- stage_former/data_structure.py
+++ stage_former/data_structure.py
@@ -1,7 +1,32 @@
 DATA_STRUCTURE = {
     "product_type": {
-        "data_path":"product_type",
+        "dir_name":"product_type",
         "table_name":"product_type",
-        "columns": {"product_type_cd":"int","product_type_nm":"text"}
+        "headers": {"product_type_cd":"int","product_type_nm":"text"}
+    },
+    "service_request": {
+        "dir_name":"service_request",
+        "table_name":"service_request",
+        "headers": {"service_request_id":"int",
+                    "customer_id":"int",
+                    "service_request_type_cd":"int",
+                    "service_request_status_cd":"int",
+                    "tail_limit":"int",
+                    "create_dttm":"text",
+                    "delete_dttm":"text"}
+    },
+    "crm_transaction":{
+        "dir_name":"crm_transaction",
+        "table_name":"crm_transaction",
+        "headers": {"transaction_id":"int",
+                    "orig_id":"int",
+                    "account_id":"int",
+                    "transaction_type_cd":"int",
+                    "transaction_amt":"double",
+                    "transaction_dttm":"datetime",
+                    "create_dttm":"datetime",
+                    "delete_dttm":"text"}
     }
+    
+
 }
--- stage_former/stage_former.py
+++ stage_former/stage_former.py
@@ -2,30 +2,64 @@ import os
 import glob
 import sys
 from pathlib import Path
+import pandas as pd
+
+sys.path.append(str(Path(__file__).parent.parent))
+import config
 
 from data_structure import DATA_STRUCTURE
 # print(DATA_STRUCTURE)
 
+def read_headers(file_path) -> list:
+    with open(file_path, 'r') as f:
+        headers = f.readline()
+    headers_list = [header.strip() for header in headers.split(",")]
+    return headers_list
+
 
 class StageFormer:
-    # data_path
-    def __init__(self, ):
-        pass
+    def __init__(self, archive_path, data_structure: dict):
+        self.archive_path = archive_path
+        self.data_structure = data_structure
 
     def get_all_dir_files(self, directory_path, extention = "csv"):
         file_paths = glob.glob(os.path.join(directory_path, f"*.{extention}"))
         return file_paths
+    
+    def process_data(self):
+        for key in self.data_structure.keys():
+            self.process_table(key)
+
+    def process_file(self, file_path, required_headers):
+        file_headers = read_headers(file_path=file_path)
+        if len(file_headers) != len(required_headers):
+            return [file_path, False, False]
+        has_headers = file_headers == required_headers
+        return [file_path, True, has_headers]
+
+    def process_table(self, key):
+        dir_name = self.data_structure[key]["dir_name"]
+        required_headers = list(self.data_structure[key]["headers"].keys())
+        print(f"{dir_name}:")
+        print(required_headers)
+        full_path = os.path.join(self.archive_path, dir_name)
+        all_files = self.get_all_dir_files(full_path)
+        table = pd.DataFrame(columns=["file_path", "success", "headers"])
+        for i, file_path in enumerate(all_files):
+            table.loc[i] = self.process_file(file_path=file_path, required_headers=required_headers)
+        print(table)
+        print("BAD DATA:")
+        failed_files = table[table["success"] == False]["file_path"]
+        if failed_files.empty:
+            print("None")
+        else:
+            print(failed_files)
 
 
 
 
 if __name__ == "__main__":
-    sys.path.append(str(Path(__file__).parent.parent))
-    import config
-
-    sf = StageFormer()
     archive_path = config.ARCHIVE_PATH
-    full_path = os.path.join(archive_path, "crm_transaction")
-
-    print(sf.get_all_dir_files(full_path))
-
+    data_structure = DATA_STRUCTURE
+    sf = StageFormer(archive_path=archive_path, data_structure=data_structure)
+    sf.process_data()
